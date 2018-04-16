d35163373fad4befab6cd7befcaa7cca73298854
camelbg <tihomir.ivanov1564@gmail.com>
2018-04-16 22:26:05 +0300

    Add DBContext, Models, Migrations and Initializer

---
+++ DataBase/Models/GradeEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace DataBase.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+}
---
+++ DataBase/UniversityContext.cs
@@ -0,0 +1,28 @@
+﻿using DataBase.Models;
+using System.Data.Entity;
+
+namespace DataBase
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+        }
+
+        public UniversityContext(string connectionString) : base (connectionString)
+        {
+            Database.SetInitializer<UniversityContext>(new UniversityInitializer<UniversityContext>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
